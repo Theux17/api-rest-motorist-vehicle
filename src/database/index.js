1af107b629a046205d8ba674aaf0279ed4ccbdbb
Theux17 <oliveira.ms18@gmail.com>
2020-11-10 03:08:37 -0300

    Ajuste na conexão com o banco de dados

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://motorist-vehicles:teste123@cluster0.wrcvg.mongodb.net/motoris-vehicles?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useFindAndModify: false
 })
 
 mongoose.Promise = global.Promise
