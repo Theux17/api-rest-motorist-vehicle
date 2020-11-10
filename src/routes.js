25aeb321a751858506d53936040cd8592cb5ee3b
Theux17 <oliveira.ms18@gmail.com>
2020-11-10 03:19:34 -0300

    Criando as rotas dos veículos

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,20 @@ const express = require('express')
 const routes = express.Router()
 
 const MotoristController = require('./controllers/MotoristController')
+const VehicleController = require('./controllers/VehicleController')
 
+// Motorist
 routes.get('/motorists', MotoristController.index)
 routes.post('/motorists', MotoristController.create)
+routes.get('/motorists/:id', MotoristController.show)
 routes.put('/motorists/:id', MotoristController.update)
 routes.delete('/motorists/:id', MotoristController.delete)
+
+// Vehicles
+routes.get('/vehicles', VehicleController.index)
+routes.post('/vehicles', VehicleController.create)
+routes.get('/vehicles/:id', VehicleController.show)
+routes.put('/vehicles/:id', VehicleController.update)
+routes.delete('/vehicles/:id', VehicleController.delete)
 
 module.exports = routes
