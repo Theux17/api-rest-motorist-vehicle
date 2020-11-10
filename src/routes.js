912ecf3dceef4c9801dcc52fed151d5411ddfc4d
Theux17 <oliveira.ms18@gmail.com>
2020-11-10 04:37:58 -0300

    Rota de adicionar veículo ao motorista

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,9 @@ routes.get('/motorists/:id', MotoristController.show)
 routes.put('/motorists/:id', MotoristController.update)
 routes.delete('/motorists/:id', MotoristController.delete)
 
+// add vehicle
+routes.post('/add-vehicles', VehicleController.addVehicle)
+
 // Vehicles
 routes.get('/vehicles', VehicleController.index)
 routes.post('/vehicles', VehicleController.create)
